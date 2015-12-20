d87edba23664e5fcfabc93a30b0188048539466d
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2015-12-20 11:37:23 +0000

    Added mysql database support

---
+++ ConnectionUControl/Database.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ConnectionUControl
+{
+    class Database
+    {
+        private string connectionString;
+        private MySqlConnection conn;
+
+        public Database(string connStr)
+        {
+            connectionString = connStr;
+            conn = null;
+
+            openConnection();
+        }
+
+        public void openConnection()
+        {
+            try
+            {
+                conn = new MySqlConnection(connectionString);
+                conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (conn != null)
+                conn.Close();
+        }
+    }
+}
